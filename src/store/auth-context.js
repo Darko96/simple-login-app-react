a9462c5bbe787544ffb7a5fdf20307e1b7f0d964
Darko Mijatovic <darkomijatovic2022@gmail.com>
2022-12-23 16:28:10 +0100

    created provider component

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
 });
+
+const AuthContextProvider = (props) => {
+  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+};
 
 export default AuthContext;
